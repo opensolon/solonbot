a41f5a55e7d4eb403b42c1d87cc1638a9b8abce5
noear <noear@live.cn>
2026-04-18 19:44:10 +0800

    优化 启动打印

--- soloncode-cli/src/main/java/org/noear/solon/codecli/portal/CliShellOld.java
+++ soloncode-cli/src/main/java/org/noear/solon/codecli/portal/CliShellOld.java
@@ -503,7 +503,7 @@ public class CliShellOld implements Runnable {
 
         String path = new File(agentRuntime.getProps().getWorkspace()).getAbsolutePath();
         // 连带版本号，紧凑排列
-        terminal.writer().println(BOLD + "SolonCode" + RESET + DIM + " " + AgentFlags.getVersion() + " PID-" + Utils.pid() + RESET + " Model:" + agentRuntime.getMainModel().getModel());
+        terminal.writer().println(BOLD + "SolonCode" + RESET + DIM + " " + AgentFlags.getVersion() + " PID-" + Utils.pid() + " Model:" + agentRuntime.getMainModel().getModel() + RESET );
         terminal.writer().println(DIM + path + RESET);
         terminal.writer().print(DIM + "Tips: " + RESET + "(esc)" + DIM + " interrupt | " +
                 RESET + "'/exit'" + DIM + ": quit | " +
