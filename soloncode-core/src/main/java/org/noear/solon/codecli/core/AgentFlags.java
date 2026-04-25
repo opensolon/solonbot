fe36402865d0a26400fc13e7078005dc519d3e18
noear <noear@live.cn>
2026-04-25 10:31:16 +0800

    v2026.4.28

--- soloncode-core/src/main/java/org/noear/solon/codecli/core/AgentFlags.java
+++ soloncode-core/src/main/java/org/noear/solon/codecli/core/AgentFlags.java
@@ -24,7 +24,7 @@ public class AgentFlags {
     public final static String VAR_MODEL_SELECTED = "_model_selected";
 
     public static String getVersion() {
-        return "v2026.4.27";
+        return "v2026.4.28";
     }
 
     private static String lastVersion;
